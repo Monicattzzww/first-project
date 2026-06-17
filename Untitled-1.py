e73640cc88635012e82775197c08d7859f9f38b0
Monica <1471146173@qq.com>
2026-06-17 14:21:45 +0800

    添加xia.py问候脚本

---
+++ Untitled-1.py
@@ -0,0 +1,3 @@
+git add .
+git commit -m "添加hello.py和更新README"
+git push origin main
--- xia.py
+++ xia.py
@@ -1,2 +1,6 @@
 def greet(name: str) -> str:
     return f"Hello, {name}! 欢迎来到我的第一个 GitHub 项目 🎉"
+
+if __name__ == "__main__":
+    user_name = input("请输入你的名字：")
+    print(greet(user_name))
